c169951589b50e380d53e79076c25934b4f8e106
quasarwei <quasar.wei@outlook.com>
2020-04-26 01:37:12 -0700

    keyboard setup

--- main.js
+++ main.js
@@ -34,80 +34,92 @@ function init() {
     1,
     90
   );
-  camera.position.set(15, 14, 16);
+  camera.position.set(0, 30, 0);
   camera.lookAt(new THREE.Vector3(0, 0, 0));
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xffffff);
   // scene.fog = new THREE.Fog(0xffffff, 15, 52);
-  scene.fog = new THREE.FogExp2(0xffffff, 0.025);
+  scene.fog = new THREE.FogExp2(0xffffff, 0.01);
 
-  let light = new THREE.HemisphereLight(0xff3f4f, 0x0080a0, 1.9); // sky color, ground color, intensity
+  let light = new THREE.HemisphereLight(0xff3f4f, 0x0080a0, 2.0); // sky color, ground color, intensity
   light.position.set(0, 8, 0);
   scene.add(light);
 
-  light = new THREE.DirectionalLight(0xe04f40, 10);
-  light.position.set(8, 25, -9);
+  light = new THREE.DirectionalLight(0xc59fb0, 5);
+  light.position.set(-25, 30, 24);
   light.target.position.set(0, 0, 0);
   light.castShadow = true;
 
-  // light.shadow.bias = -0.004;
-  light.shadow.mapSize.width = 2048;
-  light.shadow.mapSize.height = 2048;
-  light.shadow.camera.near = 1;
-  light.shadow.camera.far = 50;
-  light.shadow.radius = 10;
+  light.shadow.bias = -0.001;
+  light.shadow.mapSize.width = 4096;
+  light.shadow.mapSize.height = 4096;
+  light.shadow.camera.near = 0.1;
+  light.shadow.camera.far = 90;
+  light.shadow.radius = 2;
 
-  // light.shadow.camera.top = 10;
-  // light.shadow.camera.bottom = -10;
-  // light.shadow.camera.left = -10;
-  // light.shadow.camera.right = 10;
+  light.shadow.camera.top = 90;
+  light.shadow.camera.bottom = -90;
+  light.shadow.camera.left = -90;
+  light.shadow.camera.right = 90;
 
   scene.add(light);
   scene.add(light.target);
 
-  let newMat = new THREE.MeshStandardMaterial({
+  let keyMat = new THREE.MeshStandardMaterial({
     // color: 0x4f0b19,
-    color: 0x1a548a,
+    color: 0x1a544a,
     // emissive: 0x3e6677,
     // emissiveIntensity: -0.1,
     metalness: 0,
-    roughness: 1
+    roughness: 0.1
+  });
+
+  let blackKeyMat = new THREE.MeshStandardMaterial({
+    color: 0x050101,
+    metalness: 0,
+    roughness: 0.5
   });
 
   // ground
-  let ground = new THREE.Mesh(new THREE.PlaneBufferGeometry(150, 150), newMat);
+  let ground = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(100, 30),
+    blackKeyMat
+  );
   ground.rotation.x = -Math.PI / 2;
-  ground.position.y = -1.0;
   scene.add(ground);
   ground.receiveShadow = true;
 
-  // let grid = new THREE.GridHelper(2000, 20, 0xf00000, 0x0000f0); // size, divisions, colorCenterLine, colorGrid
-  // grid.material.opacity = 0.8;
-  // grid.material.transparent = true;
-  // scene.add(grid);
+  let grid = new THREE.GridHelper(2000, 20, 0xf00000, 0x0000f0); // size, divisions, colorCenterLine, colorGrid
+  grid.material.opacity = 0.8;
+  grid.material.transparent = true;
+  scene.add(grid);
 
-  // let dracoLoader = new DRACOLoader();
-  // dracoLoader.setDecoderPath('jsm')
   let gltfLoader = new GLTFLoader();
 
-  gltfLoader.load('cubewithplatform.glb', gltf => {
+  gltfLoader.load('minisynth.glb', gltf => {
     let model = gltf.scene;
     scene.add(model);
 
-    // model.scale.set(1, 1, 1);
+    model.scale.set(1, 1, 1);
     model.traverse(obj => {
       if (obj.castShadow !== undefined) {
         obj.castShadow = true;
         obj.receiveShadow = true;
       }
-      if (obj.isMesh) obj.material = newMat;
+      if (obj.isMesh) {
+        if (obj.name[1] == '#') {
+          obj.material = blackKeyMat;
+        } else {
+          obj.material = keyMat;
+        }
+      }
     });
 
-    mixer = new THREE.AnimationMixer(model);
-    let clip1 = gltf.animations[0];
-    let action1 = mixer.clipAction(clip1);
-    action1.play();
+    // mixer = new THREE.AnimationMixer(model);
+    // let clip1 = gltf.animations[0];
+    // let action1 = mixer.clipAction(clip1);
+    // action1.play();
     // mixer.clipAction(gltf.animations[0]).play(); // this is the same as the above three lines
   });
 
@@ -149,7 +161,7 @@ function animate() {
 
   let delta = clock.getDelta();
 
-  if (mixer) mixer.update(delta);
+  // if (mixer) mixer.update(delta);
   // controls.update(delta);
   stats.update();
 
